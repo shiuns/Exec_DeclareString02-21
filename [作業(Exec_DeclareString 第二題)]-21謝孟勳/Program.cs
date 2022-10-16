52bbd65d4f545e52c2a94fbe6961d7ca276adaee
Shiun <Shiun@DESKTOP-KPBEE4S>
2022-10-16 18:13:23 +0800

    加入專案檔案。

---
+++ [作業(Exec_DeclareString 第二題)]-21謝孟勳/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _作業_Exec_DeclareString_第二題___21謝孟勳
+{
+    //我是一個"好學生"
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("我是一個\"好學生\"");
+        }
+    }
+}
